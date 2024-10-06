5f4c5b84f3cb2f3775443e46bbd630e6c4c09a38
Vinh Hoang Dinh Vinh <hoangdvinh68@gmail.com>
2024-10-06 16:30:10 +0700

    Setup vite, antd, tailwind, eslint, prettier

---
+++ src/FastAcademy.Presentation/FastAcademy.Apps/fast-academy-web-react/src/app/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+
+export default function App() {
+  return (
+    <div>
+      <div className='text-orange-700 underline'>Fast Academy</div>
+      <Button type='primary'>Primary Button</Button>
+    </div>
+  )
+}
